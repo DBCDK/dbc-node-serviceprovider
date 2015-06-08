ba8f6f6450cb69ea396c69ba9ba4013f6b42524b
Martin Møller <mmj@dbc.dk>
2015-06-08 10:09:33 +0200

    docs added

--- src/provider.js
+++ src/provider.js
@@ -4,9 +4,17 @@ import SocketProvider from './SocketProvider.js';
 
 import * as api from './api.js';
 
+/**
+ * Initialization of the provider and the underlying services.
+ *
+ * @param {Object || null} config Object containing the necessary parameters.
+ * @param {Socket} socket If communication with the parent application should
+ * go through a socket it should be provided here. Currently there's no
+ * alternative to using socket.
+ */
 export function init(config = null, socket = null) {
   if (!config) {
-    throw new Error('No config provided');
+    throw new Error('No configuration was provided');
   }
 
   api.init(config);

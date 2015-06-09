6a1cb33dcc1e53cb96160d48e37ff229b27900ae
Martin Møller <mmj@dbc.dk>
2015-06-09 16:07:54 +0200

    - converted ES5 to ES6 - refactored one large method into several smaller

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,11 +1,13 @@
+'use strict';
+
+import {isArray} from 'lodash';
+
 /**
  * Contains all eventlisteners that should be instantiated on new connections
  * @type {Array}
  */
-'use strict';
-
-var _listeners = [];
-var _connections = [];
+let _listeners = [];
+let _connections = [];
 
 /**
  * The Dispatcher is a wrapper for socket.io. It will handle all communication
@@ -16,19 +18,50 @@ var _connections = [];
 function Dispatcher() {
 
   /**
+   * When the given promise is completed an event will be emitted.
+   *
+   * @param {Object} listener The listener object
+   * @param {Socket} connection The socket connection on which the response the
+   * event should be emitted.
+   * @param {Promise} promise A promise returned from the client.
+   */
+  function emitPromise(listener, connection, promise) {
+    promise.then((data) => {
+      connection.emit(listener.type + 'Response', data);
+    });
+  }
+
+  /**
+   * Handles the listener callback and emitting the response.
+   *
+   * @param {Object} listener The listener object
+   * @param {Object || Array} query The query object/array
+   * @param {Object || null} user The user object
+   * @param {Socket} connection The socket connection on which the response the
+   * event should be emitted.
+   */
+  function handleListenerCallback(listener, query, user, connection) {
+    const response = listener.callback(query, user);
+    if (isArray(response)) {
+      response.forEach((promise) => {
+        emitPromise(listener, connection, promise);
+      });
+    }
+    else {
+      emitPromise(listener, connection, response);
+    }
+  }
+
+  /**
    * Callback method for new connections
-   * @param  {Socket} connection a new socket connection
-   * @return {null}
+   *
+   * @param {Socket} connection a new socket connection
    */
   function makeConnection(connection) {
     var user = connection.request.session && connection.request.session.passport && connection.request.session.passport.user || null;
-    _listeners.map(function(listener) {
-      connection.on(listener.type + 'Request', function(query) {
-        listener.callback(query, user).forEach((promise) => {
-          promise.then(function(data) {
-            connection.emit(listener.type + 'Response', data);
-          });
-        });
+    _listeners.map((listener) => {
+      connection.on(listener.type + 'Request', (query) => {
+        handleListenerCallback(listener, query, user, connection);
       });
     });
   }
@@ -50,14 +83,21 @@ function Dispatcher() {
   }
 
   function getUserConnections(user) {
-    return _connections.filter(function(connection) {
+    return _connections.filter((connection) => {
       return connection.user === user;
     });
   }
 
+  /**
+   * Emits the given type of event with the given data to the given user.
+   * ¨
+   * @param {Object} user The user object.
+   * @param {String} type A string representing the event that should be emitted
+   * @param {Object || Array} data The data object returned by the client.
+   */
   function emitToUser(user, type, data) {
     var connections = getUserConnections(user);
-    connections.map(function(connection) {
+    connections.map((connection) => {
       return connection.emit(type, data);
     });
   }
@@ -80,4 +120,4 @@ function Dispatcher() {
 }
 
 // Export Dispatcher module
-module.exports = Dispatcher;
+export default Dispatcher;

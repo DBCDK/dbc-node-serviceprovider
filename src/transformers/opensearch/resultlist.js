da3e5f169a81299393bc69400884ba2e5e74d508
Rikke Lose <rikkelose@Traveller.local>
2015-06-11 10:47:05 +0200

    adding first transformer - opensearch result list

---
+++ src/transformers/opensearch/resultlist.js
@@ -0,0 +1,49 @@
+'use strict';
+
+export function transformRequest(query, offset, worksPerPage, sort) {
+
+	if (sort == "default") {
+		sort = "rank_frequency";
+	}
+  
+  return {
+  	query: query,
+  	start: offset,
+  	stepValue: worksPerPage,
+  	sort: sort
+  }
+  
+}
+
+export function transformResponse(response) {
+
+	let data = {};
+	data.result = [];
+	
+	let newWork = {};
+	
+	response.result.searchResult.forEach((work) => {
+		let no = work.collection.numberOfObjects;
+		let identifiers = [];
+		let title;
+		let workType;
+		if (no === "1") {
+			identifiers.push(work.collection.object.identifier);
+			title = work.formattedCollection.briefDisplay.manifestation.title;
+		  workType = work.formattedCollection.briefDisplay.manifestation.workType;
+		} else {
+			work.collection.object.forEach((identifier) => {
+				identifiers.push(identifier.identifier);
+				title = work.formattedCollection.briefDisplay.manifestation[0].title;
+		  	workType = work.formattedCollection.briefDisplay.manifestation[0].workType;
+			});
+		}
+		newWork.identifiers = identifiers;
+		newWork.title = title;
+		newWork.workType = workType;
+		result.data.push(newWork);
+	});
+	
+	return data;
+  
+}

e7310f3b45fb6e95b624a4a35bb8e0bdaa2d017c
Martin Møller <mmj@dbc.dk>
2015-06-11 12:36:01 +0200

    test added

---
+++ src/__tests__/dispatcher.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @file Testing the Dispatcher class in dispatcher.js
+ */
+import Dispatcher from '../dispatcher.js';
+import {expect} from 'chai';
+
+describe('Testing the methods in the Dispatcher object', () => {
+
+  it('Test handleListenerCallback', () => {
+    const dispatcher = new Dispatcher();
+    console.log(dispatcher);
+  });
+});
+
+

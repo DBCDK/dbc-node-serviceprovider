a3888c137f018b33c368d2f3548102334e7e370f
Rikke Lose <rikkelose@asavpn133.dbc.dk>
2015-06-15 10:52:20 +0200

    adding documentation + refactoring request transform

--- src/transformers/moreinfo/coverimage.js
+++ src/transformers/moreinfo/coverimage.js
@@ -1,12 +1,35 @@
 'use strict';
 
-export function transformRequest(pid) {
+import * as prep from './response-preparation.js';
+
+/**
+ * Transforms the request from the app to MoreInfo request parameters
+ *
+ * @param {Array} the pid from Open Search
+ * @return {Array} request parameters using More Info terminology
+ */
+
+export function transformRequest(request) {
+
+	let identifiers = [];
+	
+	request.forEach((value) => {
+    value.forEach((pid) => {
+    	identifiers.push(pid.pid.split(":").pop());
+    });
+  });
   
-  return {
-  	pid: pid
-  }
+  return identifiers;
   
 }
+
+/**
+ * Transforms the respone from the MoreInfo webservice to a representation 
+ * that can be used by the application
+ *
+ * @param {Object} the response from MoreInfo
+ * @return {Object} the transformed result
+ */
 
 export function transformResponse(response) {
 

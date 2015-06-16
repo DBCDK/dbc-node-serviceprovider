b4ad9e1a9d82020b86d08d569727cbf4fd561154
Martin Møller <mmj@dbc.dk>
2015-06-16 12:59:12 +0200

    minor updates to failing tests

--- src/__tests__/dispatcher.test.js
+++ src/__tests__/dispatcher.test.js
@@ -8,8 +8,13 @@ import {expect} from 'chai';
 
 describe('Testing the methods in the Dispatcher object', () => {
 
+  let dispatcher = {};
+  beforeEach(() => {
+    dispatcher = new Dispatcher();
+  });
+
   it('Test handleListenerCallback', () => {
-    const dispatcher = new Dispatcher();
+//    const dispatcher = new Dispatcher();
     console.log(dispatcher);
   });
 });
---
+++ src/__tests__/provider.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @file Testing the Dispatcher class in dispatcher.js
+ */
+import Provider from '../Provider.js';
+import {expect} from 'chai';
+
+describe('Testing methods on the Provider', () => {
+
+  it('Test handleListenerCallback', () => {
+//    const dispatcher = new Dispatcher();
+//    console.log(dispatcher);
+  });
+});
+
+

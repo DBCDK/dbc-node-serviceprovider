0d83bb2bc2db6e5c9d8394496c6163bcdefdf907
Martin Møller <mmj@dbc.dk>
2015-06-16 13:29:44 +0200

    testing the registerTransform method

--- src/__tests__/provider.test.js
+++ src/__tests__/provider.test.js
@@ -3,14 +3,34 @@
 /**
  * @file Testing the Dispatcher class in dispatcher.js
  */
-import Provider from '../Provider.js';
-import {expect} from 'chai';
+import * as Provider from '../Provider.js';
+import {expect, assert} from 'chai';
 
 describe('Testing methods on the Provider', () => {
 
-  it('Test handleListenerCallback', () => {
-//    const dispatcher = new Dispatcher();
-//    console.log(dispatcher);
+  it('assert methods are accessible', () => {
+    assert.isNotNull(Provider);
+
+    assert.isNotNull(Provider.init);
+    assert.isFunction(Provider.init);
+
+    assert.isNotNull(Provider.registerTransform);
+    assert.isFunction(Provider.registerTransform);
+  });
+
+  it('Test the registerTransform method', () => {
+    let expected = {services: null};
+    let test = {};
+    expect(Provider.registerTransform(test)).to.eql(expected);
+
+    test = {
+      someMethod(){
+      }
+    };
+    expect(Provider.registerTransform(test)).to.have.keys('services', 'someMethod');
+
+    test = {services: []};
+    expect(() => Provider.registerTransform(test)).to.throw(Error);
   });
 });
 

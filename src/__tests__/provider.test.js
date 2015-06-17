2498421d324845fb7f9eb4b9db3b7e4c4267973d
Martin Møller <mmj@dbc.dk>
2015-06-17 11:24:23 +0200

    testin errors thrown when expected methods are not available on transforms

--- src/__tests__/provider.test.js
+++ src/__tests__/provider.test.js
@@ -19,18 +19,33 @@ describe('Testing methods on the Provider', () => {
   });
 
   it('Test the registerTransform method', () => {
-    let expected = {services: null};
-    let test = {};
+    let expected = {services: null, events: true, requestTransform: true, responseTransform: true};
+    let test = {events: true, requestTransform: true, responseTransform: true};
     expect(Provider.registerTransform(test)).to.eql(expected);
 
     test = {
+      events: true,
+      requestTransform: true,
+      responseTransform: true,
       someMethod(){
       }
     };
-    expect(Provider.registerTransform(test)).to.have.keys('services', 'someMethod');
+    expect(Provider.registerTransform(test)).to.have.keys('services', 'someMethod', 'events', 'requestTransform', 'responseTransform');
 
     test = {services: []};
     expect(() => Provider.registerTransform(test)).to.throw(Error);
+
+    test = {};
+    expect(() => Provider.registerTransform(test)).to.throw(Error);
+
+    test = {events: true};
+    expect(() => Provider.registerTransform(test)).to.throw(Error);
+
+    test = {events: true, requestTransform: true};
+    expect(() => Provider.registerTransform(test)).to.throw(Error);
+
+    test = {events: true, requestTransform: true, responseTransform: true};
+    expect(() => Provider.registerTransform(test)).to.not.throw(Error);
   });
 
   it('Test the init method', () => {

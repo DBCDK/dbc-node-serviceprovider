f9cc6a744f0ea0383df8a9bdb2bf093e29c5ca82
Martin Møller <mmj@dbc.dk>
2015-06-22 10:29:51 +0200

    dist updated

--- dist/transformers/popsuggest/popsuggest.transform.js
+++ dist/transformers/popsuggest/popsuggest.transform.js
@@ -43,6 +43,25 @@ exports['default'] = Provider.registerTransform({
     }
   },
 
+  responseTransform: function responseTransform(response) {
+    var data = {};
+
+    if (response.error) {
+      data = {
+        error: true,
+        statusCode: response.error.statusCode,
+        statusMessage: response.error.statusMessage
+      };
+    } else if ((0, _lodash.isEmpty)(response.response.docs)) {
+      data.isEmpty = true;
+      data.index = this._getIndex(response);
+    } else {
+      data = this._parseData(response);
+    }
+    console.log(data);
+    return data;
+  },
+
   _getIndex: function _getIndex(response) {
     var index = '';
     if ((0, _lodash.isArray)(response.responseHeader.qf)) {
@@ -54,25 +73,63 @@ exports['default'] = Provider.registerTransform({
     return index;
   },
 
-  responseTransform: function responseTransform(response) {
-    var data = {};
-    if (response.error) {
-      data = {
-        error: true,
-        statusCode: response.error.statusCode,
-        statusMessage: response.error.statusMessage
-      };
-    } else if ((0, _lodash.isEmpty)(response.response.docs)) {
-      data.isEmpty = true;
-      data.index = this._getIndex(response);
-    } else {
-      data = {
-        index: this._getIndex(response),
-        docs: response.response.docs
-      };
+  _parseData: function _parseData(response) {
+    var index = this._getIndex(response);
+    var data = {
+      index: index,
+      docs: []
+    };
+
+    switch (index) {
+      case 'term.creator':
+        var creators = this._getCreators(response.response.docs);
+        if (creators.length >= 1) {
+          data.docs = creators;
+        }
+
+        break;
+      case 'term.title':
+        var titles = this._getTitles(response.response.docs);
+        if (titles.length >= 1) {
+          data.docs = titles;
+        }
+        break;
+      default:
+        break;
     }
 
     return data;
+  },
+
+  _getCreators: function _getCreators(docs) {
+    var creators = [];
+    var counter = 0;
+    docs.forEach(function (value) {
+      if (value['display.creator'] && counter < 5) {
+        creators.push({
+          text: value['display.creator'].join()
+        });
+        counter++;
+      }
+    });
+
+    return creators;
+  },
+
+  _getTitles: function _getTitles(docs) {
+    var titles = [];
+    var counter = 0;
+    docs.forEach(function (value) {
+      if (value['display.title'] && counter < 5) {
+        titles.push({
+          text: value['display.title'].join(),
+          img: 'http://dummyimage.com/50x50/000/fff'
+        });
+        counter++;
+      }
+    });
+
+    return titles;
   }
 });
 module.exports = exports['default'];

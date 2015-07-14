23ba68b7f2d766e81e6dbaa54f4e091b44db7207
Rikke Lose <rikkelose@Traveller.local>
2015-07-14 13:47:51 +0200

    refactoring code

--- src/transformers/opensearch/Work.transform.js
+++ src/transformers/opensearch/Work.transform.js
@@ -4,46 +4,71 @@ import * as Provider from '../../Provider.js';
 import * as prep from './response-preparation.js';
 import OpenSearch from '../../clients/OpenSearch.client.js';
 
-function getWorkData(work) {
-  let general = {};
-  if (work.collection.numberOfObjects === '1') {
-    let record = work.collection.object;
-    work.collection.object = [record];
-  }
-  let primary = work.collection.object[0].record;
-  general.title = primary.title[0];
-  if (primary.hasOwnProperty('title')) {
-    primary.title.forEach(function (title) {
-      if (title.hasOwnProperty('attributes')) {
-        if (title.attributes['xsi:type'] === 'dkdcplus:series') {
-          general.series = title.$value;
+function getRecordData(record, element, attribute, attValue) {
+  let dataElement = [];
+
+  if (record.hasOwnProperty(element)) {
+    record[element].forEach(function (elem) {
+      if (elem.hasOwnProperty('attributes')) {
+        if (elem.attributes[attribute] === attValue) {
+          dataElement.push(elem.$value);
         }
       }
     });
   }
+
+  return dataElement;
+}
+
+function getRecordDataNoAttribute(record, element) {
+  let dataElement = [];
+  if (record.hasOwnProperty(element)) {
+    record[element].forEach(function (elem) {
+      dataElement.push(elem.$value);
+    });
+  }
+
+  return dataElement;
+}
+
+function getWorkData(work) {
+  let general = {};
+  let primary = work.collection.object[0].record;
+
+  general.title = getRecordData(primary, 'title', 'xsi:type', 'dkdcplus:full')[0];
+
+  let series = getRecordData(primary, 'title', 'xsi:type', 'dkdcplus:series');
+  if (series.length > 0) {
+    general.series = series;
+  } else {
+    series = getRecordData(primary, 'description', 'xsi:type', 'dkdcplus:series');
+    if (series.length > 0) {
+      general.series = series;
+    }
+  }
+
   if (primary.hasOwnProperty('creator')) {
     let creators = [];
     primary.creator.forEach(function (creator) {
       if (!creator.hasOwnProperty('attributes')) {
-        creators.push(creator);
+        creators.push(creator.$value);
       } else if (creator.attributes['xsi:type'] !== 'oss:sort') {
         creators.push(creator.$value);
       }
     });
     general.creators = creators;
   }
-  if (primary.hasOwnProperty('abstract')) {
-    general.description = primary.abstract;
-  } else if (primary.hasOwnProperty('description')) {
-    general.description = primary.description;
-  }
-  if (primary.hasOwnProperty('description')) {
-    if (primary.description.hasOwnProperty('attributes')) {
-      if (primary.description.attributes['xsi:type'] === 'dkdcplus:series') {
-        general.series = primary.description.$value;
-      }
+
+  let description = getRecordDataNoAttribute(primary, 'abstract');
+  if (description.length > 0) {
+    general.description = description;
+  } else {
+    description = getRecordDataNoAttribute(primary, 'description');
+    if (description.length > 0) {
+      general.description = description;
     }
   }
+
   let subjects = [];
   if (primary.hasOwnProperty('subject')) {
     primary.subject.forEach(function (subject) {
@@ -84,27 +109,12 @@ function getWorkData(work) {
   if (subjects.length > 0) {
     general.subjects = subjects;
   }
-  if (primary.hasOwnProperty('hasPart')) {
-    let tracks = [];
-    primary.hasPart.forEach(function (track) {
-      if (track.hasOwnProperty('attributes')) {
-        if (track.attributes['xsi:type'] === 'dkdcplus:track') {
-          tracks.push(track.$value);
-        }
-      }
-    });
-    if (tracks.length > 0) {
-      general.tracks = tracks;
-    }
+  let tracks = getRecordData(primary, 'hasPart', 'xsi:type', 'dkdcplus:track');
+  if (tracks.length > 0) {
+    general.tracks = tracks;
   }
   if (primary.hasOwnProperty('contributor')) {
     let actors = [];
-    const cont = primary.contributor;
-    if (primary.contributor instanceof Array) {
-      primary.contributor = cont;
-    } else {
-      primary.contributor = [cont];
-    }
     primary.contributor.forEach(function (contributor) {
       if (contributor.hasOwnProperty('attributes')) {
         if (contributor.attributes['xsi:type'] === 'dkdcplus:act') {
@@ -123,7 +133,7 @@ function getWorkData(work) {
     let languages = [];
     primary.language.forEach(function (language) {
       if (!language.hasOwnProperty('attributes')) {
-        languages.push(language);
+        languages.push(language.$value);
       }
     });
     general.languages = languages;
@@ -136,15 +146,9 @@ function getManifestationData(work) {
   let specific = [];
   let types = [];
   let i = 0;
-  const manifest = work.formattedCollection.briefDisplay.manifestation;
-  if (work.formattedCollection.briefDisplay.manifestation instanceof Array) {
-    work.formattedCollection.briefDisplay.manifestation = manifest;
-  } else {
-    work.formattedCollection.briefDisplay.manifestation = [manifest];
-  }
   work.collection.object.forEach(function (manifestation) {
     const accessType = work.formattedCollection.briefDisplay.manifestation[i].accessType;
-    const type = manifestation.record.type.$value;
+    const type = manifestation.record.type[0].$value;
     if (types.indexOf(type) === -1) {
       let minorwork = {};
       types.push(type);
@@ -154,14 +158,14 @@ function getManifestationData(work) {
       identifiers.push(manifestation.identifier);
       minorwork.identifiers = identifiers;
       let dates = [];
-      dates.push(manifestation.record.date);
+      dates.push(manifestation.record.date[0].$value);
       minorwork.dates = dates;
       specific.push(minorwork);
     } else {
       specific.forEach(function (minorwork) {
         if (type === minorwork.type) {
           minorwork.identifiers.push(manifestation.identifier);
-          minorwork.dates.push(manifestation.record.date);
+          minorwork.dates.push(manifestation.record.date[0].$value);
         }
       });
     }
@@ -221,6 +225,7 @@ export default Provider.registerTransform({
     response.result.searchResult.forEach(function (work) {
 
       let newWork = {};
+      work = prep.restructureRecords(work);
       newWork.general = getWorkData(work);
       newWork.specific = getManifestationData(work);
       data.result = newWork;

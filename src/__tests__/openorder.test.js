e6ceb5df1bcc2cf45993f6603c23143c031458eb
Rikke Lose <rikkelose@dhcp106.dbc.dk>
2015-08-26 11:45:33 +0200

    transform placeOrder request and response

--- src/__tests__/openorder.test.js
+++ src/__tests__/openorder.test.js
@@ -2,7 +2,8 @@
 /*eslint-disable */
 import {expect, assert} from 'chai';
 
-const transform = require('../transformers/OpenOrder/CheckOrderPolicy.transform');
+const policyTransform = require('../transformers/OpenOrder/CheckOrderPolicy.transform');
+const orderTransform = require('../transformers/OpenOrder/PlaceOrder.transform');
 
 describe('Test transform of OpenOrder response', () => {
   
@@ -10,7 +11,7 @@ describe('Test transform of OpenOrder response', () => {
 
 		let response = {"checkOrderPolicyResponse":{"$":{"xmlns":"http://oss.dbc.dk/ns/openorder"},"lookUpUrl":["http://stormp.kk.dk/linkme.asp?26885612"],"orderPossible":["true"],"orderPossibleReason":["owned_accepted"],"orderCondition":[{"_":"Dansk betingelse","$":{"language":"dan"}},{"_":"English condition","$":{"language":"eng"}}]}, pids: [ '870970-basis:26885612' ]};
 
-		assert.equal(JSON.stringify(transform.responseTransform(response)), JSON.stringify({"result":{"canOrder":"true"},"info":{"pids":["870970-basis:26885612"]},"error":[]}), 'Can order');
+		assert.equal(JSON.stringify(policyTransform.responseTransform(response)), JSON.stringify({"result":{"canOrder":"true"},"info":{"pids":["870970-basis:26885612"]},"error":[]}), 'Can order');
 
 	});
 	
@@ -18,7 +19,7 @@ describe('Test transform of OpenOrder response', () => {
 
 		let response = {"checkOrderPolicyResponse":{"$":{"xmlns":"http://oss.dbc.dk/ns/openorder"},"lookUpUrl":["http://biblioteksbaserne.dk/sites/WODDER/pub/search.html?doaction=search&data=scode_ccl%3Did%3D28183488"],"orderPossible":["false"],"orderPossibleReason":["owned_own_catalogue"]}, pids: [ '870970-basis:28183488' ]};
 
-		assert.equal(JSON.stringify(transform.responseTransform(response)), JSON.stringify({"result":{"canOrder":"false"},"info":{"pids":["870970-basis:28183488"]},"error":[]}), 'Cannot order');
+		assert.equal(JSON.stringify(policyTransform.responseTransform(response)), JSON.stringify({"result":{"canOrder":"false"},"info":{"pids":["870970-basis:28183488"]},"error":[]}), 'Cannot order');
 
 	});
 	
@@ -26,7 +27,23 @@ describe('Test transform of OpenOrder response', () => {
 
 		let response = {"checkOrderPolicyResponse":{"$":{"xmlns":"http://oss.dbc.dk/ns/openorder"},"checkOrderPolicyError":["service_unavailable"]}, pids: [ '870970-basis:28183488' ]};
 
-		assert.equal(JSON.stringify(transform.responseTransform(response)), JSON.stringify({"result":{},"info":{"pids":["870970-basis:28183488"]},"error":[["service_unavailable"]]}), 'Error');
+		assert.equal(JSON.stringify(policyTransform.responseTransform(response)), JSON.stringify({"result":{},"info":{"pids":["870970-basis:28183488"]},"error":[["service_unavailable"]]}), 'Error');
+
+	});
+	
+	it('Response transform order placed', function() {
+
+		let response = {"placeOrderResponse":{"$":{"xmlns":"http://oss.dbc.dk/ns/openorder"},"orderPlaced":[{"orderId":["1019667320"],"orderPlacedMessage":["owned_accepted"]}],"orderCondition":[{"_":"Dansk betingelse","$":{"language":"dan"}},{"_":"English condition","$":{"language":"eng"}}]},"pids":["870970-basis:28183488"]};
+
+		assert.equal(JSON.stringify(orderTransform.responseTransform(response)), JSON.stringify({"result":{"orderPlaced":true},"info":{"pids":["870970-basis:28183488"]},"error":[]}), 'Order placed');
+
+	});
+	
+	it('Response transform order not placed', function() {
+
+		let response = {"placeOrderResponse":{"$":{"xmlns":"http://oss.dbc.dk/ns/openorder"},"orderNotPlaced":[{"placeOrderError":["service_unavailable"]}]},"pids":["870970-basis:28183481"]};
+
+		assert.equal(JSON.stringify(orderTransform.responseTransform(response)), JSON.stringify({"result":{"orderPlaced":false},"info":{"pids":["870970-basis:28183481"]},"error":[]}), 'Order not placed');
 
 	});
 

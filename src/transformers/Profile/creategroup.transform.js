b8b6fe84786a065c04e7d4957428e77010f629f0
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-09-29 15:04:40 +0200

    Added create and get group transforms

---
+++ src/transformers/Profile/creategroup.transform.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const CreateGroupTransform = {
+
+  event() {
+    return 'createGroup';
+  },
+
+  requestTransform(event, query, connection) { // eslint-disable-line no-unused-vars
+    query.email = query.name;
+
+    const passport = connection.request.session.passport || {user: {id: '', uid: ''}};
+    let loopbackGroup = {
+      accessToken: passport.user.id,
+      id: passport.user.uid
+    };
+
+    for (var i in query) {
+      if (query.hasOwnProperty(i)) {
+        loopbackGroup[i] = query[i];
+      }
+    }
+
+    return this.callServiceClient('profile', 'createGroup', loopbackProfile);
+  },
+
+  responseTransform(response, query) { // eslint-disable-line no-unused-vars
+    const isSuccesful = response.statusCode === 200;
+    return JSON.parse(isSuccesful);
+  }
+};
+
+
+export default CreateGroupTransform;

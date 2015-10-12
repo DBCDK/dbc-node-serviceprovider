d546bc761db8b951ca6b8aa173976bd43974696b
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-10-12 12:38:46 +0200

    Added vars to request transform and added status code to response

--- src/transformers/Profile/creategrouppost.transform.js
+++ src/transformers/Profile/creategrouppost.transform.js
@@ -6,16 +6,20 @@ const QueryGroupsTransform = {
   },
 
   requestTransform(event, query, connection) { // eslint-disable-line no-unused-vars
+
     const passport = connection.request.session.passport || {user: {id: '', uid: ''}};
     return this.callServiceClient('profile', 'createGroupPost', {
       title: query.title,
       content: query.content,
+      groupId: query.groupId,
+      uid: passport.user.uid,
       accessToken: passport.user.id
     });
   },
 
   responseTransform(response, query, connection) { // eslint-disable-line no-unused-vars
-    return {};
+    const isSuccesful = response.statusCode === 200;
+    return JSON.parse(isSuccesful);
   }
 };
 

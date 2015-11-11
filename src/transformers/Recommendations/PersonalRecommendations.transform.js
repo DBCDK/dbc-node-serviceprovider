1339ff570fa50350ce1a890221d5ae0f61dddefc
Jonas Peter Hyatt <jph@dbc.dk>
2015-11-11 10:18:27 +0100

    us126: refactor into extra component

--- src/transformers/Recommendations/PersonalRecommendations.transform.js
+++ src/transformers/Recommendations/PersonalRecommendations.transform.js
@@ -8,10 +8,12 @@ const PersonalRecommendationsTransform = {
   requestTransform(event, params) {
     return new Promise((resolve) => {
       let ret = {generic: [], personal: []};
+      let promises = [];
       let recs = this.callServiceClient('recommend', 'getRecommendations', {
         likes: [params.work],
         dislikes: []
       });
+      promises.push(recs);
 
       if (params.likes.length > 0) {
         params.likes.unshift(params.work);
@@ -19,6 +21,7 @@ const PersonalRecommendationsTransform = {
         let precs = this.callServiceClient('recommendranked', 'getPersonalRecommendations', {
           like: params.likes
         });
+        promises.push(precs);
 
         precs.then((res) => {
           ret.personal = res.result;
@@ -27,6 +30,9 @@ const PersonalRecommendationsTransform = {
 
       recs.then((res) => {
         ret.generic = res.result;
+      });
+
+      Promise.all(promises).then((values) => {
         resolve(ret);
       });
     });

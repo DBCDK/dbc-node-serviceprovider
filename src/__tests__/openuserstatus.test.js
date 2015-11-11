918d0640207c6586bdbc035092f3be3fe1692004
rikkelose <rikkelose@gmail.com>
2015-11-11 13:27:28 +0100

    Formatting due date. I was working on sd-112

--- src/__tests__/openuserstatus.test.js
+++ src/__tests__/openuserstatus.test.js
@@ -36,8 +36,7 @@ describe('Test transform of OpenUserStatus response', () => {
 	it('Response transform 2 loaned items', function() {
 
 		let response = {"ous:getUserStatusResponse":{"$":{"xmlns:ous":"http://oss.dbc.dk/ns/openuserstatus","xmlns":"http://oss.dbc.dk/ns/openuserstatus"},"ous:userId":["1231231231"],"ous:userStatus":[{"ous:loanedItems":[{"ous:loan":[{"ous:author":["Hesel, Lene Ewald"],"ous:title":["Pokker ta' den rådne kat"],"ous:dateDue":["2015-11-24T00:00:00+01:00"],"ous:loanId":["5008917428"],"ous:reminderLevel":["1"]},{"ous:author":["Davis, Jim"],"ous:title":["Garfield på bølgen blå"],"ous:dateDue":["2015-11-24T00:00:00+01:00"],"ous:loanId":["3487223157"],"ous:reminderLevel":["1"]}],"ous:loansCount":["2"]}],"ous:orderedItems":[{"ous:ordersCount":["0"]}],"ous:fiscalAccount":[{"ous:totalAmount":["0"],"ous:totalAmountCurrency":["DKK"]}]}]}};
-
-		assert.equal(JSON.stringify(userStatusTransform.responseTransform(response)), JSON.stringify({"result":{"orderedItems":{"count":0,"orders":[]},"loanedItems":{"count":2,"loans":[{"author":"Hesel, Lene Ewald","title":"Pokker ta\' den rådne kat","dueDate":"2015-11-24T00:00:00+01:00","loanId":"5008917428"},{"author":"Davis, Jim","title":"Garfield på bølgen blå","dueDate":"2015-11-24T00:00:00+01:00","loanId":"3487223157"}]},"fiscalAccount":{"totalAmount":0,"currency":"DKK","items":[]}},"info":{"userId":"1231231231"},"error":[]}), '2 loaned items');
+		assert.equal(JSON.stringify(userStatusTransform.responseTransform(response)), JSON.stringify({"result":{"orderedItems":{"count":0,"orders":[]},"loanedItems":{"count":2,"loans":[{"author":"Hesel, Lene Ewald","title":"Pokker ta\' den rådne kat","dueDate":"24/11-2015","loanId":"5008917428"},{"author":"Davis, Jim","title":"Garfield på bølgen blå","dueDate":"24/11-2015","loanId":"3487223157"}]},"fiscalAccount":{"totalAmount":0,"currency":"DKK","items":[]}},"info":{"userId":"1231231231"},"error":[]}), '2 loaned items');
 
 	});
 

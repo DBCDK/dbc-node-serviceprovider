61da30eb84f576b318693fba13a9d7dc4cd96681
Jonas Peter Hyatt <jph@dbc.dk>
2016-01-11 10:41:05 +0100

    us136: socketcluster

--- src/lib/dispatcher.js
+++ src/lib/dispatcher.js
@@ -7,22 +7,6 @@
  */
 
 /**
- * Register events when a new connections is made.
- *
- * @param connection
- * @param provider
- */
-function registerEventOnConnection(transform, connection) {
-  const event = transform.event();
-  connection.on(`${event}Request`, (request) => {
-    transform.trigger(request, connection).forEach((responsePromise) => {
-      responsePromise
-        .then(response => connection.emit(`${event}Response`, response))
-        .catch(error => connection.emit(`${event}Response`, {error}));
-    });
-  });
-}
-/**
  * Register events from the provider on new connections
  *
  * @param socket
@@ -34,7 +18,7 @@ export default function Dispatcher(transforms, logger, io) {
   io.on('connection', function(connection) {
     transforms.forEach((transform) => {
       const event = transform.event();
-      connection.on(`${event}Request`, (data, res) => {
+      connection.on(`${event}Request`, (data) => {
         transform.trigger(data, connection).forEach((responsePromise) => {
           responsePromise
             .then((response) => {

b648e5824dc5e66371c44759a72a45b877383665
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-02-04 13:51:30 +0100

    Added additional logging, for profiling. Also fixed wrong quoting in previous commit. (" should be ' to be consistent)

--- src/lib/Dispatcher.js
+++ src/lib/Dispatcher.js
@@ -10,15 +10,25 @@
  * Register events when a new connections is made.
  *
  * @param connection
+ * @param logger
  * @param provider
  */
-function registerEventOnConnection(transform, connection) {
+function registerEventOnConnection(transform, logger, connection) {
   const event = transform.event();
   connection.on(`${event}Request`, (request) => {
+    logger.log('debug', `${event}Request`, request);
+    var startTime = Date.now();
     transform.trigger(request, connection).forEach((responsePromise) => {
       responsePromise
-        .then(response => connection.emit(`${event}Response`, response))
-        .catch(error => connection.emit(`${event}Response`, {error}));
+        .then(response => {
+          logger.log('debug', `${event}Response`, response);
+          logger.log('info', `${event}Response time`, Date.now() - startTime);
+          connection.emit(`${event}Response`, response);
+        })
+        .catch(error => {
+          logger.log('warning', `Error in ${event}Response`, error);
+          connection.emit(`${event}Response`, {error});
+        });
     });
   });
 }
@@ -32,8 +42,8 @@ function registerEventOnConnection(transform, connection) {
  */
 export default function Dispatcher(transforms, logger, io) {
   // On socket.io it would make more sense to use `io.use(...)` instead of
-  // `io.on("connection"...)`, but io.use is not supported on socketcluster yet.
-  io.on("connection", (connection) => {
-    transforms.forEach((transform) => registerEventOnConnection(transform, connection));
+  // `io.on('connection'...)`, but io.use is not supported on socketcluster yet.
+  io.on('connection', (connection) => {
+    transforms.forEach((transform) => registerEventOnConnection(transform, logger, connection));
   });
 }

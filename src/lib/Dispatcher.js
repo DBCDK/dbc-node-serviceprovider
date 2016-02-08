857d594c00f6279b60b4bff1c0aa6bf5ada162c9
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-02-08 07:17:41 +0100

    logging: add timing and error-string to error log. Remove to verbose debug log.

--- src/lib/Dispatcher.js
+++ src/lib/Dispatcher.js
@@ -16,19 +16,18 @@
 function registerEventOnConnection(transform, logger, connection) {
   const event = transform.event();
   connection.on(`${event}Request`, (request) => {
-    logger.log('debug', `${event}Request`, request);
     var startTime = Date.now();
     transform.trigger(request, connection).forEach((responsePromise) => {
       responsePromise
         .then(response => {
-          logger.log('debug', `${event}Response`, response);
           logger.log('info', `${event}Response time`, Date.now() - startTime);
           connection.emit(`${event}Response`, response);
         })
         .catch(error => {
           // in-progress/TODO make emitted/logged error contain more info
           // - we cannot emit the error itself, as it may be a cyclic structure
-          logger.log('warning', `ResponseError`, event);
+          logger.log('warning', 'ResponseError', {event: event,
+            time: Date.now() - startTime, 'String(error)': String(error)});
           connection.emit(`${event}Response`, {error: 'server error'});
         });
     });
